b0c3f7dd7ddaa32897522666b1e3a5e59841ad6b
Ivan Zimin <154788803+seo-stuff@users.noreply.github.com>
2024-01-22 13:27:48 +0300

    Add files via upload

--- slug-analyzer 2.py
+++ slug-analyzer 2.py
@@ -7,9 +7,22 @@ from openpyxl import load_workbook
 
 # Определение кодировки файла
 def detect_encoding(file_path):
-    with open(file_path, 'rb') as f:
-        result = chardet.detect(f.read())
-    return result['encoding']
+    encodings_to_try = ['utf-8', 'cp1251']  # Попробуйте эти кодировки в указанном порядке
+    detected_encoding = None
+    
+    for encoding in encodings_to_try:
+        try:
+            with open(file_path, 'r', encoding=encoding) as f:
+                f.read()
+            detected_encoding = encoding
+            break
+        except UnicodeDecodeError:
+            continue
+    
+    if detected_encoding is None:
+        raise Exception('Не удалось определить кодировку файла')
+    
+    return detected_encoding
 
 # Получение кодировки файла
 file_encoding = detect_encoding('import.csv')
@@ -42,11 +55,11 @@ for index, row in df.iterrows():
     
     for i, slug in enumerate(slugs):
         if i + 1 not in nested_slug_stats:
-            nested_slug_stats[i + 1] = {'Слаг': [], 'Количество': [], 'Сум.видимость': [], 'Глубина': [], 'Пример URL': []}
+            nested_slug_stats[i + 1] = {'Слаг': [], 'Сум.видимость': [], 'Количество': [], 'Глубина': [], 'Пример URL': []}
         
         nested_slug_stats[i + 1]['Слаг'].append(slug)
-        nested_slug_stats[i + 1]['Количество'].append(1)
         nested_slug_stats[i + 1]['Сум.видимость'].append(visibility)
+        nested_slug_stats[i + 1]['Количество'].append(1)
         nested_slug_stats[i + 1]['Глубина'].append(i + 1)
         nested_slug_stats[i + 1]['Пример URL'].append(url)
 
@@ -55,15 +68,13 @@ export_file = 'export.xlsx'
 with pd.ExcelWriter(export_file, engine='openpyxl') as writer:
     # Создаем первый лист со всеми данными
     df_all = pd.concat([pd.DataFrame(data) for data in nested_slug_stats.values()], ignore_index=True)
-    df_all = df_all.groupby('Слаг').agg({'Количество': 'sum', 'Сум.видимость': 'sum', 'Глубина': 'first', 'Пример URL': 'first'}).reset_index()
+    df_all = df_all.groupby('Слаг').agg({'Сум.видимость': 'sum', 'Количество': 'sum', 'Глубина': 'first', 'Пример URL': 'first'}).reset_index()
     df_all = df_all.sort_values(by='Сум.видимость', ascending=False)
-    df_all['% от общего'] = round((df_all['Сум.видимость'] / df_all['Сум.видимость'].sum()) * 100, 1)  # Округляем проценты
-    df_all = df_all[['Слаг', 'Сум.видимость', 'Количество', 'Глубина', 'Пример URL', '% от общего']]  # Переупорядочиваем колонки
     df_all.to_excel(writer, index=False, sheet_name='Статистика')
     worksheet = writer.sheets['Статистика']
     
     # Задаем ширину колонок
-    column_widths = [20, 20, 20, 20, 30, 20]  # Ширина колонок
+    column_widths = [20, 20, 20, 20, 30]  # Ширина колонок
     for i, column in enumerate(worksheet.columns):
         max_length = max(len(str(cell.value)) for cell in column)
         worksheet.column_dimensions[column[0].column_letter].width = column_widths[i]
@@ -71,10 +82,8 @@ with pd.ExcelWriter(export_file, engine='openpyxl') as writer:
     # Создаем отдельные листы для каждого уровня вложенности
     for nesting_level, data in nested_slug_stats.items():
         df_nesting_level = pd.DataFrame(data)
-        df_nesting_level = df_nesting_level.groupby('Слаг').agg({'Количество': 'sum', 'Сум.видимость': 'sum', 'Глубина': 'first', 'Пример URL': 'first'}).reset_index()
+        df_nesting_level = df_nesting_level.groupby('Слаг').agg({'Сум.видимость': 'sum', 'Количество': 'sum', 'Глубина': 'first', 'Пример URL': 'first'}).reset_index()
         df_nesting_level = df_nesting_level.sort_values(by='Сум.видимость', ascending=False)
-        df_nesting_level['% от общего'] = round((df_nesting_level['Сум.видимость'] / df_all['Сум.видимость'].sum()) * 100, 1)
-        df_nesting_level = df_nesting_level[['Слаг', 'Сум.видимость', 'Количество', 'Глубина', 'Пример URL', '% от общего']]
         df_nesting_level.to_excel(writer, index=False, sheet_name=f'Вложенность {nesting_level}')
         worksheet = writer.sheets[f'Вложенность {nesting_level}']
 
